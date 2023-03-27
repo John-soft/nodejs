53104cc505744ca12c82cc9dca33734afe0cb7be
John-soft <akinwalereoluwaseun60@gmail.com>
2023-03-27 11:48:26 +0100

    Another new commit

--- app.js
+++ app.js
@@ -15,7 +15,12 @@
 // npm init (step by step , press enter to skip)
 // npm init -y (everything default)
 
+const lodash = require('lodash');
 
+const items = [1,[2,[3,[4]]]]
+
+const newItems = lodash.flattenDeep(items);
+console.log(array);
 
 
 
