f70838c754e605bf1f35ad26e975cb05e90db9be
John-soft <akinwalereoluwaseun60@gmail.com>
2023-03-28 16:19:43 +0100

    received new update

--- app.js
+++ app.js
@@ -24,6 +24,76 @@
 // console.log("Hello Fatherjohn");
 
 
+// const fs = require('fs');
+
+// console.log("Started first task");
+// //Check file path
+
+// fs.readFile('./content/info.txt', 'utf8',(err, data) => {
+//     if(err){
+//         console.log(err);
+//         return;
+//     }else{
+//         console.log(data);
+//         console.log("Completed the first task");
+//     }
+// });
+
+// console.log('Starting next task');
+
+
+//started operating system process
+// console.log('first');
+// setTimeout(()=>{
+//     console.log('second');
+// }, 0);
+// console.log('third');
+//completed and exited the operating process
+
+
+
+// setInterval(() =>{
+//     console.log("Hello world");
+// }, 2000);
+
+// console.log("I will run first");
+
+
+// const http = require('http');
+
+// const server = http.createServer((req, res) =>{
+//     console.log('request event');
+//     res.end('hello');
+// }).listen(5000, ()=>{
+//     console.log("Server listening on port : 5000....");
+// });
+
+
+const fs = require('fs');
+
+function getText(path) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, 'utf8', (err, data)=>{
+            if (err) {
+                reject(err);
+            }else{
+                resolve(data);
+            }
+        })
+    })
+}
+
+getText('./content/info.txt')
+.then((result => console.log(result)))
+.catch((err) => console.log(err));
+
+
+
+
+
+
+
+
 
 
 
