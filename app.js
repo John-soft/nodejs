04503f3c5e8faa78aaba46df6e392adb95aee64f
John-soft <akinwalereoluwaseun60@gmail.com>
2023-03-31 12:14:48 +0100

    New update

--- app.js
+++ app.js
@@ -115,14 +115,19 @@
 
 // customEmitter.emit('response');
 
-const fs = require('fs');
+// const fs = require('fs');
 
-for (let i = 0; i < 10000; i++) {
-    console.log('Hello world');    
-}
+// for (let i = 0; i < 10000; i++) {
+//     fs.writeFileSync("./content/big.txt", `Hello world ${i}\n`, {flag: "a"});    
+// }
 
 
+const {createReadStream} = require('fs')
 
+const stream = createReadStream('./content/big.txt', {highWaterMark: 90000, encoding: 'utf-8'});
+stream.on('data', (result)=>{
+    console.log(result);
+});
 
 
 
